ff6bf55ca4adf306cd9047d6acf360c6ee6371b5
nonk123 <Sergeyii321@gmail.com>
2023-01-15 00:58:09 +0300

    Initialize the repository

---
+++ QuantizeRenders.ps1
@@ -0,0 +1,9 @@
+$palettePath = 'models/Palette.png'
+$ditherMethod = 'None'
+
+$files = Get-ChildItem -Recurse -Path 'models/renders'
+
+foreach ($file in $files) {
+    $fileName = $file.FullName
+    magick $fileName -dither $ditherMethod -remap $palettePath $fileName
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# midgets
+
+A GZDoom gameplay modification that forces you to overcome your vertical challenges.
